a17f09a2d70ef6a8b28e817089bbd266a983d3f9
Dmitry Kaspiarovich <claywhoami@yandex.ru>
2018-12-11 20:04:13 +0300

    added Procfile

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGO_URL: "mongodb://127.0.0.1/carsell",
+    MONGO_URL: "mongodb://justmad1:justmad11@ds131784.mlab.com:31784/carsell",
+    MONGO_URL1: "mongodb://127.0.0.1/carsell",
     SESSION_SECRET: "KURSACH"
 }
